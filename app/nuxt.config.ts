56a4d376ac8f2cfc40c10404c933637a909eba89
Mardiya TCHACKEY <elearning617@gmail.com>
2024-07-11 11:41:07 +0000

    firebase config: change of api key

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
       firebase: {
         apiKey: process.env.API_KEY,
         authDomain: process.env.AUTH_DOMAIN,
-        databaseURL: process.env.DA,
+        databaseURL: process.env.DATABASE_URL,
         projectId: process.env.PROJECT_ID,
         storageBucket: process.env.STORAGE_BUCKET,
         messagingSenderId: process.env.MESSAGING_SENDER_ID,
