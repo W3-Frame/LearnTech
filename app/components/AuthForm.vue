62c027c2c806f341df4a04888c40768ca6acf0e9
Mardiya TCHACKEY <elearning617@gmail.com>
2024-07-14 07:17:08 +0000

    creation of components of login page

---
+++ app/components/AuthForm.vue
@@ -0,0 +1,53 @@
+<template>
+    <form class="login-form" @submit.prevent="onSubmit">
+      <div class="form-group">
+        <label>Username</label>
+        <input type="text" placeholder="Enter username" class="form-control" v-model="state.username" />
+        <i class="far fa-envelope"></i>
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input type="password" placeholder="Enter password" class="form-control" v-model="state.password" />
+        <i class="fas fa-lock"></i>
+      </div>
+      <div class="form-group d-flex align-items-center justify-content-between">
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="remember-me" v-model="state.rememberMe" />
+          <label for="remember-me" class="form-check-label">Remember Me</label>
+        </div>
+        <a href="#" class="forgot-btn">Forgot Password?</a>
+      </div>
+      <div class="form-group">
+        <button type="submit" class="login-btn">Login</button>
+      </div>
+    </form>
+  </template>
+  
+  <script setup>
+  import { reactive } from 'vue';
+  
+  const state = reactive({
+    username: '',
+    password: '',
+    rememberMe: false,
+  });
+  
+  const onSubmit = () => {
+    console.log("Form Submitted", state);
+  };
+  </script>
+  
+  <style scoped>
+  .login-form .form-control {
+    position: relative;
+    padding-right: 30px;
+  }
+  
+  .login-form .form-control + i {
+    position: absolute;
+    right: 10px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  </style>
+  
---
+++ app/components/GoogleSignIn.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul class="login-social">
+        <!-- <li><a href="#" class="bg-fb"><i class="fab fa-facebook-f"></i></a></li>
+        <li><a href="#" class="bg-twitter"><i class="fab fa-twitter"></i></a></li> -->
+        <li><a href="#" class="bg-gplus" @click="signInWithGoogle"><i class="fab fa-google-plus-g"></i></a></li>
+        <!-- <li><a href="#" class="bg-git"><i class="fab fa-github"></i></a></li> -->
+    </ul>
+</template>
+  
+  <script setup>
+  import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+  import { getAdditionalUserInfo } from "firebase/auth";
+  import { doc, setDoc } from "firebase/firestore";
+  import { auth, db } from "~/utils/firebase";
+  
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, new GoogleAuthProvider()).then(async (result) => {
+      const isNewUser = getAdditionalUserInfo(result)?.isNewUser;
+      const { email, displayName, uid } = result.user;
+  
+      if (isNewUser) {
+        await setDoc(doc(db, "users", uid), { email, displayName });
+      }
+    });
+  };
+  </script>
+  
