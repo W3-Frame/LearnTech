825a7a65ab1a77768557d54d6ddce603ba5baad1
Hermann EHO <hermannleboss@gmail.com>
2024-07-15 12:19:01 +0000

    feat: dashboard layout

---
+++ app/layouts/dashboard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+useHead({
+  bodyAttrs: {
+    class: 'dark:bg-gray-950'
+  }
+})
+</script>
+
+<template>
+  <div class="h-screen flex">
+    <slot/>
+  </div>
+</template>
+
+<style scoped>
+</style>
