44d196d6768e8699fd068b6b86d8d876a097023e
Mardiya TCHACKEY <elearning617@gmail.com>
2024-07-16 16:24:56 +0000

    creation file for lessons crud

---
+++ app/pages/lessons/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts">
+import { useFirestore } from '@vueuse/firebase';
+import { collection } from 'firebase/firestore';
+
+definePageMeta({
+  layout: "dashboard",
+});
+useSeoMeta({
+  title: "Lessons Page",
+});
+
+const { db } = useFirebase();
+const Course = useFirestore(collection(db, "Course"));
+
+</script>
+
+<style scoped>
+
+</style>
