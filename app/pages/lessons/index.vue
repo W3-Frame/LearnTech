ddd2027a2d2eca2ead282232cf2c626139a6f550
Mardiya TCHACKEY <elearning617@gmail.com>
2024-07-23 08:40:29 +0000

    crud lesson start

--- app/pages/lessons/index.vue
+++ app/pages/lessons/index.vue
@@ -1,7 +1,24 @@
 <template>
+  <div>
     <div>
+      <h1>Les matières enseignées</h1>
+      <!-- <div class="row">
+        <div class="col-md-4" v-for="matiere in matieres" :key="matiere.id">
 
+          <div class="card" style="width: 18rem; margin: 10px>
+
+    <img class=" card-img-top" :src="matiere.image" alt="Card image>
+
+    <div class=" card-body">
+
+            <h5 class="card-title">{{ matiere.nom }}</h5>
+            <p class="card-text">{{ matiere.description }}</p>
+
+          </div>
+        </div>
+      </div> -->
     </div>
+  </div>
 </template>
 
 <script lang="ts">
