33ca1cfc413b660a7b28d3d64fa2f49bf9d44c1f
Mardiya TCHACKEY <elearning617@gmail.com>
2024-07-25 14:21:23 +0000

    creation de la page de tableau de bord

---
+++ app/pages/userInterfaces/homepage/dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+     <h1>Tableau de bord</h1>
+   </div>
+</template>
+
+<script setup>
+definePageMeta({
+ layout: "userboard",
+});
+
+useSeoMeta({
+ title: "Tableau de bord",
+});
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
+ 
