649bb09c56c8ef52d5190bf45e9a21c1418b262b
Mardiya TCHACKEY <elearning617@gmail.com>
2024-07-25 14:21:06 +0000

    creation de la page des cours

---
+++ app/pages/userInterfaces/homepage/courses.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+     <h1>Mes cours</h1>
+   </div>
+</template>
+
+<script setup>
+definePageMeta({
+ layout: "userboard",
+});
+
+useSeoMeta({
+ title: "Mes cours",
+});
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
+ 
