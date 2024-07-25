744d95aac9b38b6faf938d35414719293c0a75a1
Mardiya TCHACKEY <elearning617@gmail.com>
2024-07-25 14:20:51 +0000

    creation de la page d'accueil

---
+++ app/pages/userInterfaces/homepage/accueil.vue
@@ -0,0 +1,24 @@
+<template>
+     <div>
+      <h1>Accueil</h1>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "userboard",
+});
+
+useSeoMeta({
+  title: "Accueil",
+});
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
+  
