e799a3e6342fe0dded0101044f95d0d761507970
Mardiya TCHACKEY <elearning617@gmail.com>
2024-07-25 14:21:37 +0000

    creation de la page des notes

---
+++ app/pages/userInterfaces/homepage/grades.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+     <h1>Mes notes</h1>
+   </div>
+</template>
+
+<script setup>
+definePageMeta({
+ layout: "userboard",
+});
+
+useSeoMeta({
+ title: "Mes notes",
+});
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
+ 
